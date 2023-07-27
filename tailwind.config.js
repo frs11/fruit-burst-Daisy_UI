0c4e875feffa7d960eb1d0adeb70d40a1275a9af
frs11~ <fayezurrahman017@gmail.com>
2023-07-27 17:54:51 +0600

    meet our team section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'team-bg': "url('./images/team.png')",
+        }
+      },
     },
     plugins: [],
   }
